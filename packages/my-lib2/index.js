b4617e2177f41fcc78728fc5196b3960426ee339
Guillaume Chervet <guillaume.chervet@axa.fr>
2018-09-19 20:13:40 +0000

    maj

--- packages/my-lib2/index.js
+++ packages/my-lib2/index.js
@@ -1,3 +1,3 @@
 const mylib1 = require('@guillaume.chervet/my-lib1');
 
-exports.FonctionEncorePlusimportante = () => mylib1.Fonctionimportante() + 2;
+exports.FonctionEncorePlusimportante = () => mylib1.Fonctionimportante() + 3;
